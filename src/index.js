60422326ab101ada368fae4774910eb0724553cb
Ole Edvard Berg Leren <oebl89@gmail.com>
2022-11-21 21:16:30 +0100

    Initial attempt to initialize kartograph map-object with svg-file.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import * as $K from "kartograph-js";
+import * as $K from "../node_modules/kartograph-js";
+import * as config from "./project.config";
+import { mapFullPath } from "./project.config";
 
 let map = $K.map("#map")
+map.loadMap(mapFullPath(), () => {
+    console.log("Map Loaded.");
+});
---
+++ src/project.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+     mapName: "doskvol_thedocks_outline.svg",
+    mapDirectory: "./assets/",
+    mapFullPath: () => this.mapDirectory + this.mapName
+}
